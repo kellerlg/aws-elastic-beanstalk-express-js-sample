665c11a94940ac9d6d2206612775e44fd3004a20
kellerlg <55753607+kellerlg@users.noreply.github.com>
2020-10-01 13:17:17 -0400

    change message

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 8080;
 
 <<<<<<< HEAD
-app.get('/', (req, res) => res.send('Hello cruel World!'));
+app.get('/', (req, res) => res.send('Goodbye cruel World!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
